bb922b5298167a784664f386843c30362d0a7ad9
Daria <dariaurbanik@gmail.com>
2019-05-03 16:04:28 +0200

    React project created

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react';
+
+
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state ={
+      friends: [],
+      picture: "",
+      name: "",
+  };
+}
+
+    updatePicture(value){
+      this.setState({picture: value});
+    }
+
+    updateName(value){
+      this.setState({name: value});
+    }
+  
+    addFriend(){
+      const {friends, picture, name} = this.state;
+
+      let newFriends = friends.slice();
+      newFriends.push({picture, name});
+
+      this.setState({friends: newFriends, picture: "", name:""});
+    }
+  
+  render() {
+    const friends = this.state.friends.map((friend, index) => (
+      <div key={ `friend-${index}-${friend.name}`}>
+      <img width="100px" src={friend.picture} />
+      <span>{friend.name}</span>
+      </div>
+    ));
+    
+  return (
+    <div>
+      <label>Picture:</label>
+      <input onChange={ (e) => this.updatePicture(e.target.value)} value={ this.state.picture }/>
+      <label>Name:</label>
+      <input onChange={(e) => this.updateName(e.target.value)} value={ this.state.name }/>
+      <button onClick={() => this.addFriend()}>Add Friend</button>
+      
+      {friends}
+    </div>
+  );
+}
+}
+
+export default App;
